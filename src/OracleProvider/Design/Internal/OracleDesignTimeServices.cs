d12d76f84245734c203a3e532871e8e6453f9d60
焰尾迭 <353166224@qq.com>
2018-05-15 22:24:19 +0800

    1

---
+++ src/OracleProvider/Design/Internal/OracleDesignTimeServices.cs
@@ -0,0 +1,30 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Oracle.Scaffolding.Internal;
+using Microsoft.EntityFrameworkCore.Oracle.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.EntityFrameworkCore.Oracle.Design.Internal
+{
+    /// <summary>
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class OracleDesignTimeServices : IDesignTimeServices
+    {
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public virtual void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+            => serviceCollection
+                .AddSingleton<IScaffoldingProviderCodeGenerator, OracleScaffoldingCodeGenerator>()
+                .AddSingleton<IDatabaseModelFactory, OracleDatabaseModelFactory>()
+                .AddSingleton<IAnnotationCodeGenerator, OracleAnnotationCodeGenerator>()
+                .AddSingleton<IRelationalTypeMapper, OracleTypeMapper>();
+    }
+}
---
+++ src/OracleProvider/Utilities/MemberInfoExtensions.cs
@@ -0,0 +1,34 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+
+namespace System.Reflection
+{
+    internal static class MemberInfoExtensions
+    {
+        public static Type GetMemberType(this MemberInfo memberInfo)
+            => (memberInfo as PropertyInfo)?.PropertyType ?? ((FieldInfo)memberInfo)?.FieldType;
+
+        public static bool IsSameAs(this MemberInfo propertyInfo, MemberInfo otherPropertyInfo)
+        {
+            if (propertyInfo == null)
+            {
+                return otherPropertyInfo == null;
+            }
+
+            if (otherPropertyInfo == null)
+            {
+                return false;
+            }
+
+            return Equals(propertyInfo, otherPropertyInfo)
+                   || (propertyInfo.Name == otherPropertyInfo.Name
+                       && (propertyInfo.DeclaringType == otherPropertyInfo.DeclaringType
+                           || propertyInfo.DeclaringType.GetTypeInfo().IsSubclassOf(otherPropertyInfo.DeclaringType)
+                           || otherPropertyInfo.DeclaringType.GetTypeInfo().IsSubclassOf(propertyInfo.DeclaringType)
+                           || propertyInfo.DeclaringType.GetTypeInfo().ImplementedInterfaces.Contains(otherPropertyInfo.DeclaringType)
+                           || otherPropertyInfo.DeclaringType.GetTypeInfo().ImplementedInterfaces.Contains(propertyInfo.DeclaringType)));
+        }
+    }
+}
