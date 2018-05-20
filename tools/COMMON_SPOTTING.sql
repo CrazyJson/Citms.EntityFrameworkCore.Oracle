9515c5231aba79f0baf57d95de2f1de6f044b902
焰尾迭 <353166224@qq.com>
2018-05-20 18:25:28 +0800

    1

---
+++ tools/COMMON_SPOTTING.sql
@@ -0,0 +1,75 @@
+﻿---------------------------数据字典生成工具(V1.0)--------------------------------
+declare  tableExist number;
+begin
+    select count(1) into tableExist from user_tables where upper(table_name)=upper('COMMON_SPOTTING') ;
+    if tableExist = 0  then
+        execute immediate '
+            CREATE TABLE COMMON_SPOTTING(
+                SPOTTINGID VARCHAR2(50)  NOT NULL  ,
+                SPOTTINGNO VARCHAR2(50)  NOT NULL  ,
+                SPOTTINGNAME NVARCHAR2(100)  NOT NULL  ,
+                UNIQUECODE VARCHAR2(50)   ,
+                ROADID VARCHAR2(50)   ,
+                LONGITUDE NUMBER(12,8)   ,
+                LATITUDE NUMBER(12,8)   ,
+                DEPARTMENTID VARCHAR2(50)  NOT NULL  ,
+                SOURCEKIND VARCHAR2(50) DEFAULT ''local''   NOT NULL  ,
+                CREATOR VARCHAR2(50)  NOT NULL  ,
+                CREATEDTIME DATE DEFAULT sysdate   NOT NULL  ,
+                MODIFIER VARCHAR2(50)   ,
+                MODIFIEDTIME DATE   ,
+                FLAGS VARCHAR2(10)   ,
+                REMARK NVARCHAR2(500)   ,
+                APPLICATIONNAME VARCHAR2(50) DEFAULT ''Citms.PIS''   NOT NULL  ,
+                AREACODE VARCHAR2(50)   ,
+                BOPOMOFO VARCHAR2(200)   ,
+                SPOTTINGTYPE VARCHAR2(50)   ,
+                VIRTUALDELETEFLAG INTEGER DEFAULT 0   ,
+                DISABLED NUMBER(1,0) DEFAULT 0   ,
+                PUNISHDEPARTMENT VARCHAR2(50)   ,
+                DIVISIONCODE VARCHAR2(50)   ,
+                APPROVESTATUS INTEGER DEFAULT 0   ,
+                APPROVEUSERID VARCHAR2(50)   ,
+                APPROVETIME DATE   ,
+                APPROVEINFO NVARCHAR2(200)   ,
+                MAXWEIGHT NUMBER(12,4)   ,
+                MAXHEIGHT NUMBER(12,4)   ,
+		PRIMARY KEY(SPOTTINGID) 
+            )
+	';
+
+        execute immediate 'comment ON TABLE  COMMON_SPOTTING IS ''道路点位表''';
+
+        execute immediate 'comment on column COMMON_SPOTTING.SPOTTINGID is ''点位ID''';
+        execute immediate 'comment on column COMMON_SPOTTING.SPOTTINGNO is ''点位编号(可以为厂家分配的点位编号)''';
+        execute immediate 'comment on column COMMON_SPOTTING.SPOTTINGNAME is ''点位名称''';
+        execute immediate 'comment on column COMMON_SPOTTING.UNIQUECODE is ''上传六合一标准代码''';
+        execute immediate 'comment on column COMMON_SPOTTING.ROADID is ''所在道路ID''';
+        execute immediate 'comment on column COMMON_SPOTTING.LONGITUDE is ''经度坐标值''';
+        execute immediate 'comment on column COMMON_SPOTTING.LATITUDE is ''纬度坐标值''';
+        execute immediate 'comment on column COMMON_SPOTTING.DEPARTMENTID is ''所在管理部门''';
+        execute immediate 'comment on column COMMON_SPOTTING.SOURCEKIND is ''来源类型''';
+        execute immediate 'comment on column COMMON_SPOTTING.CREATOR is ''创建用户ID''';
+        execute immediate 'comment on column COMMON_SPOTTING.CREATEDTIME is ''创建时间''';
+        execute immediate 'comment on column COMMON_SPOTTING.MODIFIER is ''修改人''';
+        execute immediate 'comment on column COMMON_SPOTTING.MODIFIEDTIME is ''修改时间''';
+        execute immediate 'comment on column COMMON_SPOTTING.FLAGS is ''保留标记''';
+        execute immediate 'comment on column COMMON_SPOTTING.REMARK is ''备注''';
+        execute immediate 'comment on column COMMON_SPOTTING.APPLICATIONNAME is ''应用名称''';
+        execute immediate 'comment on column COMMON_SPOTTING.AREACODE is ''所属辖区代码''';
+        execute immediate 'comment on column COMMON_SPOTTING.BOPOMOFO is ''拼音简称''';
+        execute immediate 'comment on column COMMON_SPOTTING.SPOTTINGTYPE is ''点位类型(字典表字典 ，Kind 为 1003 ， 十字路口/丁字路口/圆形转盘/其它)''';
+        execute immediate 'comment on column COMMON_SPOTTING.VIRTUALDELETEFLAG is ''逻辑删除标记(0 正常数据， 1 逻辑删除)''';
+        execute immediate 'comment on column COMMON_SPOTTING.DISABLED is ''是否停用(0 未停用， 1 停用)，默认为0''';
+        execute immediate 'comment on column COMMON_SPOTTING.PUNISHDEPARTMENT is ''处理单位''';
+        execute immediate 'comment on column COMMON_SPOTTING.DIVISIONCODE is ''行政区划代码''';
+        execute immediate 'comment on column COMMON_SPOTTING.APPROVESTATUS is ''审核状态(0:未审核, 1:审核通过, 2:审核未通过), 默认为未审核状态''';
+        execute immediate 'comment on column COMMON_SPOTTING.APPROVEUSERID is ''审核用户代码''';
+        execute immediate 'comment on column COMMON_SPOTTING.APPROVETIME is ''审核时间''';
+        execute immediate 'comment on column COMMON_SPOTTING.APPROVEINFO is ''审核说明''';
+        execute immediate 'comment on column COMMON_SPOTTING.MAXWEIGHT is ''最大限重(KG)''';
+        execute immediate 'comment on column COMMON_SPOTTING.MAXHEIGHT is ''最大限高(m)''';
+        
+    end if;
+end;
+	
---
+++ tools/SYS_DEPARTMENT.sql
@@ -0,0 +1,64 @@
+﻿---------------------------数据字典生成工具(V1.0)--------------------------------
+declare  tableExist number;
+begin
+    select count(1) into tableExist from user_tables where upper(table_name)=upper('SYS_DEPARTMENT') ;
+    if tableExist = 0  then
+        execute immediate '
+            CREATE TABLE SYS_DEPARTMENT(
+                DEPARTMENTID VARCHAR2(32) DEFAULT sys_guid()   NOT NULL  ,
+                BUNAME NVARCHAR2(50)   ,
+                BUFULLNAME NVARCHAR2(100)   ,
+                BUCODE NVARCHAR2(50)   ,
+                HIERARCHYCODE NVARCHAR2(500)   ,
+                PARENTGUID VARCHAR2(32)   ,
+                WEBSITE NVARCHAR2(50)   ,
+                FAX NVARCHAR2(20)   ,
+                COMPANYADDR NVARCHAR2(100)   ,
+                CHARTER NVARCHAR2(50)   ,
+                CORPORATIONDEPUTY NVARCHAR2(20)   ,
+                CREATEDON DATE   ,
+                MODIFIEDON DATE   ,
+                CREATEDBY VARCHAR2(32)   ,
+                COMMENTS NVARCHAR2(500)   ,
+                MODIFIEDBY VARCHAR2(32)   ,
+                ISENDCOMPANY NUMBER(1,0) DEFAULT 0   ,
+                ISCOMPANY NUMBER(1,0) DEFAULT 0   ,
+                BULEVEL INTEGER DEFAULT 0   ,
+                BUTYPE NUMBER(3,0) DEFAULT 0   ,
+                ORDERCODE NVARCHAR2(20)   ,
+                ORDERHIERARCHYCODE NVARCHAR2(500)   ,
+                AREACODE VARCHAR2(10)   ,
+                SIMPLECODE NVARCHAR2(50)   
+		            )
+	';
+
+        execute immediate 'comment ON TABLE  SYS_DEPARTMENT IS ''组织机构表''';
+
+        execute immediate 'comment on column SYS_DEPARTMENT.DEPARTMENTID is ''单位GUID''';
+        execute immediate 'comment on column SYS_DEPARTMENT.BUNAME is ''单位简称''';
+        execute immediate 'comment on column SYS_DEPARTMENT.BUFULLNAME is ''单位全称''';
+        execute immediate 'comment on column SYS_DEPARTMENT.BUCODE is ''单位代码''';
+        execute immediate 'comment on column SYS_DEPARTMENT.HIERARCHYCODE is ''层级代码''';
+        execute immediate 'comment on column SYS_DEPARTMENT.PARENTGUID is ''父级GUID''';
+        execute immediate 'comment on column SYS_DEPARTMENT.WEBSITE is ''网址''';
+        execute immediate 'comment on column SYS_DEPARTMENT.FAX is ''传真''';
+        execute immediate 'comment on column SYS_DEPARTMENT.COMPANYADDR is ''公司地址''';
+        execute immediate 'comment on column SYS_DEPARTMENT.CHARTER is ''营业执照''';
+        execute immediate 'comment on column SYS_DEPARTMENT.CORPORATIONDEPUTY is ''法人代表''';
+        execute immediate 'comment on column SYS_DEPARTMENT.CREATEDON is ''创建时间''';
+        execute immediate 'comment on column SYS_DEPARTMENT.MODIFIEDON is ''修改时间''';
+        execute immediate 'comment on column SYS_DEPARTMENT.CREATEDBY is ''创建人''';
+        execute immediate 'comment on column SYS_DEPARTMENT.COMMENTS is ''说明''';
+        execute immediate 'comment on column SYS_DEPARTMENT.MODIFIEDBY is ''修改人''';
+        execute immediate 'comment on column SYS_DEPARTMENT.ISENDCOMPANY is ''是否末级公司''';
+        execute immediate 'comment on column SYS_DEPARTMENT.ISCOMPANY is ''是否公司''';
+        execute immediate 'comment on column SYS_DEPARTMENT.BULEVEL is ''层级数''';
+        execute immediate 'comment on column SYS_DEPARTMENT.BUTYPE is ''组织类型''';
+        execute immediate 'comment on column SYS_DEPARTMENT.ORDERCODE is ''排序代码''';
+        execute immediate 'comment on column SYS_DEPARTMENT.ORDERHIERARCHYCODE is ''排序层级代码''';
+        execute immediate 'comment on column SYS_DEPARTMENT.AREACODE is ''单位所属区域编码''';
+        execute immediate 'comment on column SYS_DEPARTMENT.SIMPLECODE is ''单位简码''';
+        
+    end if;
+end;
+	
