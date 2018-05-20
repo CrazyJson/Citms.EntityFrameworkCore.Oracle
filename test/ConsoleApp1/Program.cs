d6b187a3d516e56177db07b420f0c99927281d71
焰尾迭 <353166224@qq.com>
2018-05-20 21:26:19 +0800

    修改测试代码

--- test/ConsoleApp1/Program.cs
+++ test/ConsoleApp1/Program.cs
@@ -11,22 +11,28 @@ namespace ConsoleApp1
         {
             using (CommonDBContext db = new CommonDBContext())
             {
-                //int i = db.Spotting.Count();
+                int total = db.Spotting.Count();
+                Console.WriteLine("路口总行数：", total);
+                var fDisItem = db.Spotting.FirstOrDefault(e => e.Disabled == true);
+                Console.WriteLine("第一条禁用路口：", fDisItem.SpottingName);
+                //分页查询演示
+                var pageList = db.Spotting.Where(e => e.Disabled == true)
+                    .OrderBy(e => e.SpottingName).Skip(10).Take(20).ToList();
+                Console.WriteLine("分页查询禁用路口：", pageList.Count);
+                var list = db.Spotting.ToList();
+                string[] arrSpottingNo = new string[] { "123", "34" };
+                db.Spotting.Where(e => arrSpottingNo.Contains(e.SpottingNo)).ToList();
 
-                //var cc1 = db.Spotting.FirstOrDefault(e => e.Disabled == true);
-                //var cc = db.Spotting.Where(e => e.Disabled == true).OrderBy(e => e.SpottingName).Skip(10).Take(20).ToList();
-                //var list = db.Spotting.ToList();
-                //string[] aa = new string[] { "123", "34" };
-                //db.Spotting.Where(e => aa.Contains(e.SpottingNo)).ToList();
-
-                //var item = db.Spotting.Find("a5563b53d23548179ed857ac3820df73");
-                var item = db.Spotting.AsNoTracking().FirstOrDefault(e => e.SpottingId == "a5563b53d23548179ed857ac3820df73");
-                //string cc2 = db.Spotting.Min(e => e.SpottingNo);
-                //string cc3 = db.Spotting.Max(e => e.SpottingNo);
-                //string[] cc4 = db.Spotting.Select(e=>e.AreaCode).Distinct().ToArray();
-                //db.Spotting.Average(e => e.Longitude);
+                var item = db.Spotting.Find("a5563b53d23548179ed857ac3820df73");
+                var itemNoTrack = db.Spotting.AsNoTracking().FirstOrDefault(e => e.SpottingId == "a5563b53d23548179ed857ac3820df73");
+                string minSpottingNo = db.Spotting.Min(e => e.SpottingNo);
+                string maxSpottingNo = db.Spotting.Max(e => e.SpottingNo);
+                string[] arrAreaCode = db.Spotting.Select(e => e.AreaCode).Distinct().ToArray();
+                db.Spotting.Average(e => e.Longitude);
                 var dt = DateTime.Now.AddDays(-100);
-                db.Spotting.Where(e => e.Createdtime >= dt && e.Createdtime <= DateTime.Now && e.Disabled == true).ToList();
+                //日期过滤
+                db.Spotting.Where(e => 
+                    e.Createdtime >= dt && e.Createdtime <= DateTime.Now && e.Disabled == true).ToList();
                 item.SpottingName = "test";
                 item.Createdtime = DateTime.Now;
                 item.SpottingId = Guid.NewGuid().ToString("N");
@@ -34,7 +40,7 @@ namespace ConsoleApp1
 
                 var dItem = db.Spotting.Find(item.SpottingId);
                 db.Remove(dItem);
-
+                //关联查询
                 var x = (from p in db.Spotting
                          join q in db.Department
                          on p.DepartmentId equals q.DepartmentId
@@ -43,7 +49,6 @@ namespace ConsoleApp1
 
                 db.SaveChanges();
             }
-            Console.WriteLine("Hello World!");
             Console.Read();
         }
     }
