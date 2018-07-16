93334f7e95da9b4b1bbd171efe2ca96a47150e58
焰尾迭 <353166224@qq.com>
2018-07-16 17:37:36 +0800

    1

--- test/ConsoleApp1/Program.cs
+++ test/ConsoleApp1/Program.cs
@@ -11,11 +11,6 @@ namespace ConsoleApp1
         {
             using (CommonDBContext db = new CommonDBContext())
             {
-                db.Database.Query<Spotting>("select * from common_spotting where spottingId=:a", 
-                    new { a = "123" });
-                db.Database.ToSingle<Spotting>("select * from common_spotting where spottingId=:a",
-                    new { a = "123" });
-
                 int total = db.Spotting.Count();
                 Console.WriteLine("路口总行数：{0}", total);
                 var fDisItem = db.Spotting.FirstOrDefault(e => e.Disabled == true);
@@ -28,39 +23,16 @@ namespace ConsoleApp1
                 string[] arrSpottingNo = new string[] { "123", "34" };
                 db.Spotting.Where(e => arrSpottingNo.Contains(e.SpottingNo)).ToList();
 
-<<<<<<< .mine
 
 
                 string minSpottingNo = db.Spotting.Min(e => e.SpottingNo);
                 string maxSpottingNo = db.Spotting.Max(e => e.SpottingNo);
                 string[] arrAreaCode = db.Spotting.Select(e => e.AreaCode).Distinct().ToArray();
                 db.Spotting.Average(e => e.Longitude);
-=======
-                var item = db.Spotting.Find("a5563b53d23548179ed857ac3820df73");
-                var itemNoTrack = db.Spotting.AsNoTracking().FirstOrDefault(e => e.SpottingId == "a5563b53d23548179ed857ac3820df73");
-                string minSpottingNo = db.Spotting.Min(e => e.SpottingNo);
-                string maxSpottingNo = db.Spotting.Max(e => e.SpottingNo);
-                string[] arrAreaCode = db.Spotting.Select(e => e.AreaCode).Distinct().ToArray();
-                db.Spotting.Average(e => e.Longitude);
->>>>>>> .theirs
                 var dt = DateTime.Now.AddDays(-100);
-<<<<<<< .mine
                 //日期过滤
                 db.Spotting.Where(e =>
                     e.Createdtime >= dt && e.Createdtime <= DateTime.Now && e.Disabled == true).ToList();
-
-
-
-
-=======
-                //日期过滤
-                db.Spotting.Where(e =>
-                    e.Createdtime >= dt && e.Createdtime <= DateTime.Now && e.Disabled == true).ToList();
-                item.SpottingName = "test";
-                item.Createdtime = DateTime.Now;
-                item.SpottingId = Guid.NewGuid().ToString("N");
-                db.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Added;
->>>>>>> .theirs
 
 
                 var itemNew = new Spotting
@@ -82,13 +54,8 @@ namespace ConsoleApp1
 
                 var dItem = db.Spotting.Find(itemNew.SpottingId);
                 db.Remove(dItem);
-<<<<<<< .mine
                 Console.WriteLine("删除路口Id：{0} 数据", itemNew.SpottingId);
 
-=======
-                //关联查询
-
->>>>>>> .theirs
                 //关联查询
                 var x = (from p in db.Spotting
                          join q in db.Department
